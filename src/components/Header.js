86e6319d3ac7134b88173ad2fce2dab20b7b802e
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-08-02 15:29:33 -0300

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,32 +1,14 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-//import Icon from 'react-icons/fa';    
-//'react-native-vector-icons/FontAwesome';
-import { FaFontAwesome } from 'react-icons/fa';
+import './Header.css'; // Importe o arquivo CSS
+import { FaBars } from 'react-icons/fa';
 
 const Header = ({ title }) => {
   return (
-    <View style={styles.header}>
-      <Text style={styles.title}>{title}</Text>
-   <Icon name="bars" size={24} color="white" />
-   <FaFontAwesome />
-    </View>
+    <header className="header">
+      <h1 className="title">{title}</h1>
+      <FaBars className="icon" />
+    </header>
   );
 };
-
-const styles = StyleSheet.create({
-  header: {
-    backgroundColor: '#009739',
-    padding: 15,
-    flexDirection: 'row',
-    justifyContent: 'space-between',
-    alignItems: 'center',
-  },
-  title: {
-    color: 'white',
-    fontSize: 20,
-    fontWeight: 'bold',
-  },
-});
 
 export default Header;
