352fadbdb92fe40e174ea6f39d123ea94e17e33c
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-08-02 16:09:59 -0300

    Update Chatbot.js

--- src/components/Chatbot.js
+++ src/components/Chatbot.js
@@ -2,8 +2,8 @@ import React, { useState, useRef, useEffect } from 'react';
 import './Chatbot.css'; // Importe o arquivo CSS
 import axios from 'axios';
 import { useLocation, useNavigate } from 'react-router-dom';
-import lessons from './screens/lessons'; // Importe o arquivo lessons.js
-import exercises from './screens/exercises'; // Importe o arquivo exercises.js
+import lessons from './screens/Lessons'; // Importe o arquivo lessons.js
+import exercises from './screens/Exercises'; // Importe o arquivo exercises.js
 
 const Chatbot = () => {
   const [messages, setMessages] = useState([]);
