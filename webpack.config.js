b08c92b60ccbd9f3e0755b20b7d0f76b4108db4a
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-08-02 14:54:30 -0300

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,3 @@
-const webpack = require('webpack');
 const path = require('path');
 
 module.exports = {
@@ -6,5 +5,28 @@ module.exports = {
     alias: {
       'react-native$': 'react-native-web'
     }
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-react']
+          }
+        }
+      }
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    compress: true,
+    port: 9000
   }
 };
