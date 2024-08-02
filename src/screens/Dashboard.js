4594aaf2edb8baead535c0a64a85a3afb27dc18b
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-08-02 15:10:01 -0300

    Update Dashboard.js

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,30 +1,17 @@
 import React from 'react';
-import { View, Text, Button, StyleSheet } from 'react-native';
+import './Dashboard.css'; // Importe o arquivo CSS
 
 const Dashboard = ({ navigation }) => {
   return (
-    <View style={styles.container}>
-      <Text style={styles.title}>Painel de Controle</Text>
-      <Button title="Aulas" onPress={() => navigation.navigate('Lessons')} color="#009739" />
-      <Button title="Exercícios" onPress={() => navigation.navigate('Exercises')} color="#009739" />
-      <Button title="Chatbot" onPress={() => navigation.navigate('Chatbot')} color="#009739" />
-      <Button title="Perfil" onPress={() => navigation.navigate('Profile')} color="#009739" />
-      <Button title="Redação" onPress={() => navigation.navigate('Redacao')} color="#009739" />
-    </View>
+    <div className="container">
+      <h1 className="title">Painel de Controle</h1>
+      <button className="button" onClick={() => navigation.navigate('Lessons')}>Aulas</button>
+      <button className="button" onClick={() => navigation.navigate('Exercises')}>Exercícios</button>
+      <button className="button" onClick={() => navigation.navigate('Chatbot')}>Chatbot</button>
+      <button className="button" onClick={() => navigation.navigate('Profile')}>Perfil</button>
+      <button className="button" onClick={() => navigation.navigate('Redacao')}>Redação</button>
+    </div>
   );
 };
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-  },
-  title: {
-    fontSize: 24,
-    fontWeight: 'bold',
-    marginBottom: 20,
-  },
-});
 
 export default Dashboard;
