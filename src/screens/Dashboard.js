2acc3e99cb3e3aafa0e692984b9681d8e99b7b3e
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-08-08 03:18:12 -0300

    Update Dashboard.js

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import { doc, getDoc, collection, getDocs } from 'firebase/firestore';
-import { db } from './firebase';
+import { db } from './components/firebase';
 
 const Dashboard = () => {
   const [user, setUser] = useState(null);
