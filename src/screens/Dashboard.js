9c1da97fdc85f861eda8db9ca1d909ca830108c3
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-08-08 03:01:16 -0300

    Update Dashboard.js

--- src/screens/Dashboard.js
+++ src/screens/Dashboard.js
@@ -1,20 +1,54 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import './Dashboard.css'; // Importe o arquivo CSS
+import { useState, useEffect } from 'react';
+import { doc, getDoc, collection, getDocs } from 'firebase/firestore';
+import { db } from './firebase';
 
 const Dashboard = () => {
-  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [enemData, setEnemData] = useState(null);
+
+  useEffect(() => {
+    const userDocRef = doc(db, "Users", user.uid);
+    const enemDataRef = collection(userDocRef, "EnemData");
+
+    const loadUserData = async () => {
+      try {
+        const userDoc = await getDoc(userDocRef);
+        setUser(userDoc.data());
+
+        const enemDataQuery = await getDocs(enemDataRef);
+        const enemDataDocs = enemDataQuery.docs;
+        const enemData = enemDataDocs[0].data();
+        setEnemData(enemData);
+      } catch (error) {
+        console.error(error);
+      }
+    };
+
+    loadUserData();
+  }, []);
 
   return (
-    <div className="container">
-      <h1 className="title">Painel de Controle</h1>
-      <button className="button" onClick={() => navigate('Lessons')}>Aulas</button>
-      <button className="button" onClick={() => navigate('Exercises')}>Exercícios</button>
-      <button className="button" onClick={() => navigate('Chatbot')}>Chatbot</button>
-      <button className="button" onClick={() => navigate('Profile')}>Perfil</button>
-      <button className="button" onClick={() => navigate('redacao')}>Redação</button>
+    <div>
+      <h1>Dashboard</h1>
+      {user && (
+        <div>
+          <h2>Informações do Usuário</h2>
+          <p>Email: {user.email}</p>
+          <p>Nome: {user.firstName}</p>
+        </div>
+      )}
+      {enemData && (
+        <div>
+          <h2>Dados do Enem</h2>
+          <p>Espanhol: {enemData.espanhol.acertos} acertos, {enemData.espanhol.erros} erros</p>
+          <p>Inglês: {enemData.Ingles.acertos} acertos, {enemData.Ingles.erros} erros</p>
+          <p>Língua Portuguesa: {enemData.ling.acertos} acertos, {enemData.ling.erros} erros</p>
+          <p>Matemática: {enemData.matematica.acertos} acertos, {enemData.matematica.erros} erros</p>
+          <p>Ciências Humanas: {enemData.cienciasHumanas.acertos} acertos, {enemData.cienciasHumanas.erros} erros</p>
+          <p>Ciências da Natureza: {enemData.cienciasDaNatureza.acertos} acertos, {enemData.cienciasDaNatureza.erros} erros</p>
+          <p>Redação: {enemData.redacao.notaTotal} nota total, {enemData.redacao.redacoesFeitas} redações feitas</p>
+        </div>
+      )}
     </div>
   );
 };
-
-export default Dashboard;
