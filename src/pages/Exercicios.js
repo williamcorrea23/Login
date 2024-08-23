681a68d607e452febfefa835428432494a5e15ef
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-08-23 17:43:15 -0300

    Update Exercicios.js

--- src/pages/Exercicios.js
+++ src/pages/Exercicios.js
@@ -18,7 +18,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const db = getFirestore(app);
 
-const apiKey = env.apikey;
+const apiKey = process.env.API_KEY;
 
 const subjects = [
   "Ciências Naturais",
