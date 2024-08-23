a6a55f749a2b9922955def2bd7e7188709f361bf
williamcorrea23 <58587242+williamcorrea23@users.noreply.github.com>
2024-08-23 14:46:05 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './styles/global.css';
+import './styles/globals.css';
 import App from './App';
 import { BrowserRouter as Router } from 'react-router-dom';
 
